ea5e6bc713fad42574f030aa1c2ab8cc8a82843d
Matthew Wicks <mwicks7@gmail.com>
2023-04-20 16:51:22 -0400

    Resolves dns error

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,1 @@
-import { useState } from 'react'
-import ShopLayout from '@/components/layout/shopLayout'
-import StickyContainer from '@/components/global/stickyContainer'
-import Hero from '@/components/global/hero'
-import Sort from '@/components/category/sort'
-import Products from '@/components/category/products'
-import Breadcrumbs from '@/components/global/breadcrumbs'
-import Filters from '@/components/category/filters'
-import { getProducts, getCategories } from '@/lib/dbProducts'
-
-export async function getServerSideProps(context) {}
-
-
 export default function Home() {}
