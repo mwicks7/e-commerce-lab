fdb8ae7e7faadc0753891f543474e73ea85a14a3
Matthew Wicks <mwicks7@gmail.com>
2023-04-22 19:59:26 -0400

    Moves sort to aside

--- components/forms/select.js
+++ components/forms/select.js
@@ -2,8 +2,8 @@
 export default function Select({ label, options, onChange }) {
   return (
     <>
-      <span>{label} &nbsp;</span>
-      <select className="sort__input" onChange={onChange}>
+      <label className="select__label">{label} &nbsp;</label>
+      <select className="select__input" onChange={onChange}>
         {options.map(option => (
           <option key={option.value} value={option.value}>{option.name}</option>
         ))}
