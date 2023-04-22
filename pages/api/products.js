a41d15fce41196c747239313a6e80eba3833a65e
Matthew Wicks <mwicks7@gmail.com>
2023-04-22 17:54:57 -0400

    Styling updates and adds related products

--- pages/api/products.js
+++ pages/api/products.js
@@ -2,7 +2,7 @@ import { getProducts } from '@/lib/dbProducts'
 
 export default async function handler(req, res) {
   const requestData = JSON.parse(req.body)
-  const responseData = await getProducts(requestData.sort, requestData.filters)
+  const responseData = await getProducts(requestData.sort, requestData.filters, requestData.limit)
   res.status(200).json(responseData)
 }
 
--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,19 +1,17 @@
 import { useState, useCallback, useEffect, useRef } from 'react'
+import { getCategories } from '@/lib/dbCategories'
+import { getProducts } from '@/lib/dbProducts'
 import ShopLayout from '@/components/layout/shopLayout'
 import StickyContainer from '@/components/global/stickyContainer'
-import Image from 'next/image'
-import Sort from '@/components/category/sort'
-import Products from '@/components/category/products'
 import Breadcrumbs from '@/components/global/breadcrumbs'
-import Filters from '@/components/category/filters'
-import { getCategories } from '@/lib/dbCategories'
-import { getProduct } from '@/lib/dbProducts'
+import Gallery from '@/components/global/gallery'
+import Product from '@/components/category/product'
 
 export async function getServerSideProps({ params }) {
-  const productData = await getProduct(params.slug)
+  const productData = await getProducts({}, { slug: params.slug }, 1)
   const categories = await getCategories()
   const currentCat = categories.filter(cat => cat.slug === productData[0].category)[0]
-
+  
   return {
     props: { 
       categories, 
@@ -24,7 +22,25 @@ export async function getServerSideProps({ params }) {
   }
 }
 
-export default function Product({ categories, currentCat, productData }) {
+
+
+export default function Products({ categories, currentCat, productData }) {
+  const [relatedProducts, setRelatedProducts] = useState([])
+
+  useEffect(() => {
+      fetch(
+        '/api/products', { 
+        method: 'POST',
+        body: JSON.stringify({
+          sort: {},
+          filters: { category: productData.category },
+          limit: 3
+        })
+      })
+        .then( (response) => response.json() )
+        .then( (data) => setRelatedProducts(data))
+  }, [productData])
+
   return (
     <ShopLayout
       pageSlug={currentCat.slug}
@@ -42,25 +58,40 @@ export default function Product({ categories, currentCat, productData }) {
       </StickyContainer>
 
       <div className="container container--main">
-        <div className="grid grid--aside-right">
+        <section className="grid grid--aside-right">
 
           <div className="grid__main">
-            <Image src={productData.images[0]} alt="" width={1000} height={1000}/>
+            <Gallery
+              images={productData.images}
+              height={1000}
+              width={1000}
+            />
           </div>
           
           <aside className="grid__aside grid__aside--lrg">
-            <h1>{productData.name}</h1>
+            <h1 className="h1">{productData.name}</h1>
             <p>${productData.price.toLocaleString("en-US")}</p>
             <p>{productData.description}</p>
             {Object.entries(productData.filters).map(detail => (
               <p key={detail[0]}>
-                <b>{detail[0]}</b> {detail[1]}
+                <b>{detail[0]}:</b> {detail[1]}
               </p>
             ))}
-            <p><button>Add to cart</button></p>
+            <p><button className="button">Add to cart</button></p>
           </aside>
-        </div>
+        </section>
       </div>
+      <div className="container container--main">
+        <section className="products">
+          <h3>You may also like</h3>
+          <div className="products__grid">
+            {relatedProducts.map(rp => (
+              <Product key={rp._id + 'rp'} product={rp} />
+            ))}
+          </div>
+        </section>
+      </div>
+      
     </ShopLayout>
   )
 }
