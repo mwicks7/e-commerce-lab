c3b34c2a943bcc309d3f8274d8dd759bb34286ca
Matthew Wicks <mwicks7@gmail.com>
2023-04-26 07:35:01 -0400

    A11y updates

--- components/forms/select.js
+++ components/forms/select.js
@@ -2,12 +2,14 @@
 export default function Select({ label, options, onChange }) {
   return (
     <>
-      <label className="select__label">{label} &nbsp;</label>
-      <select className="select__input" onChange={onChange}>
-        {options.map(option => (
-          <option key={option.value} value={option.value}>{option.name}</option>
-        ))}
-      </select>
+      <label>
+        <span className="select__label">{label}</span>
+        <select className="select__input" onChange={onChange}>
+          {options.map(option => (
+            <option key={option.value} value={option.value}>{option.name}</option>
+          ))}
+        </select>
+      </label>
     </>
   )
 }
